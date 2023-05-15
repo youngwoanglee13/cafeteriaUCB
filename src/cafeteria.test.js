51d84d71ab3cc77312d2ff9b81547be603683400
younglee <young.lee@ucb.edu.bo>
2023-05-15 15:27:57 -0400

    IMPLEMENTAR VER DISPONIBILIDAD DEL MENU

--- src/cafeteria.test.js
+++ src/cafeteria.test.js
@@ -5,5 +5,10 @@ describe("Sumar", () => {
     const cafeteria = new Cafeteria();
     expect(cafeteria.getProductos().length).toEqual(0);
   });
-
+  it("deberia devolver cantidad de productos disponibles", () => {
+    const cafeteria = new Cafeteria();
+    cafeteria.cargarProductos();
+    expect(cafeteria.getProductos().length).toEqual(4);
+  });
+  
 });
