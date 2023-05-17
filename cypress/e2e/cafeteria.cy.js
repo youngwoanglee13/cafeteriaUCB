7748b0d5ae81fbc0d6b32cc5d3b5ad1dfebd2bf9
younglee <young.lee@ucb.edu.bo>
2023-05-17 19:13:37 -0400

    CYPRESS VER PRODUCTOS POR CATEGORIA

--- cypress/e2e/cafeteria.cy.js
+++ cypress/e2e/cafeteria.cy.js
@@ -1,6 +1,11 @@
 describe("Cafeteria", () => {
   it("Muestra todos los productos disponibles en el menu", () => {
     cy.visit("/");
-    cy.get('#menu-cafeteria').children('li').should('have.length', 4);
+    cy.get('#menu-cafeteria').children('li').should('have.length', 5);
+  });
+  it("Muestra productos con categoria almuerzo", () => {
+    cy.visit("/");
+    cy.get('select#idcategoria').select('almuerzo');
+    cy.get('#menu-cafeteria').children('li').should('have.length', 2);
   });
 });
