8e5f7760026ab470f0672b9475050ea5f6ca7183
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-05-31 23:30:16 -0400

    TDD ELIMINAR UNA RESERVA YA CREADA

--- src/cafeteria.test.js
+++ src/cafeteria.test.js
@@ -161,4 +161,12 @@ describe("Cafeteria", () => {
     expect(cafeteria.getProductos("todas")[0].stock).toEqual(stockInicial + cantidadAumento);
   });
 
+  it("deberia eliminar una reserva", () => {
+    const cafeteria = new Cafeteria();
+    cafeteria.cargarProductos();
+    cafeteria.hacerReserva(3,5,"detalle");
+    expect(cafeteria.getReservas().length).toEqual(1);
+    cafeteria.eliminarReserva(cafeteria.getReservas()[0].id);
+    expect(cafeteria.getReservas().length).toEqual(0);
+  });
 });
